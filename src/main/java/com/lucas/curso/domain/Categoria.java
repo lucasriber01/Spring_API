7d46798b6b16093aba46ff25ac9b02ebc2548896
Jehan Lucas <lucasarago.df@gmail.com>
2020-08-24 00:42:48 -0300

    Implementação do H2

--- src/main/java/com/lucas/curso/domain/Categoria.java
+++ src/main/java/com/lucas/curso/domain/Categoria.java
@@ -2,11 +2,20 @@ package com.lucas.curso.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+
+@Entity
 public class Categoria implements Serializable{
 	
 
 	
 	private static final long serialVersionUID = 1L;
+	
+	@javax.persistence.Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer Id ; 
 	private String nome; 
 	
