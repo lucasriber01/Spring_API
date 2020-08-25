7789ad1f6ed8e2575f6bc1b959a3cfff3481bdea
Jehan Lucas <lucasarago.df@gmail.com>
2020-08-25 02:34:21 -0300

    Repository Categoria

--- src/main/java/com/lucas/curso/resources/CategoriaResource.java
+++ src/main/java/com/lucas/curso/resources/CategoriaResource.java
@@ -3,32 +3,33 @@ package com.lucas.curso.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucas.curso.domain.Categoria;
+import com.lucas.curso.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	
-	@RequestMapping(method= RequestMethod.GET)
+	@Autowired
+	private CategoriaService service ; 
+	
+	@RequestMapping(value ="/{id}" ,  method= RequestMethod.GET)
 
-	public List<Categoria> listar() {
+	public ResponseEntity<?> find(@PathVariable  Integer id) {
 		
-		Categoria cat1 = new Categoria(1, "Informatica");
-				
-		Categoria cat2 = new Categoria(2, "Escritorio");
+		Categoria obj = service.buscar(id);
 		
-		List<Categoria> lista = new ArrayList<>();
 		
-		lista.add(cat1);
-		lista.add(cat2);
+		return ResponseEntity.ok().body(obj) ;
 
-		
-		return lista;
 	}
 
 }
