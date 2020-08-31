bbbdaa5bd8e623723728b6c10336ab18e05200e5
Jehan Lucas <lucasarago.df@gmail.com>
2020-08-31 16:35:51 -0300

    UPDATE

--- src/main/java/com/lucas/curso/resources/CategoriaResource.java
+++ src/main/java/com/lucas/curso/resources/CategoriaResource.java
@@ -27,7 +27,7 @@ public class CategoriaResource {
 
 	public ResponseEntity<?> find(@PathVariable  Integer id) {
 		
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 		
 		
 		return ResponseEntity.ok().body(obj) ;
@@ -44,5 +44,12 @@ public class CategoriaResource {
 	}
 	
 	
+	@RequestMapping(value ="/{id}" ,  method= RequestMethod.PUT)
+	public ResponseEntity<Categoria> update(@RequestBody Categoria obj , @PathVariable  Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+		
+	}
 
 }
