9486fb525b3d92fa90a232a8e237011d45f9a529
Jehan Lucas <lucasarago.df@gmail.com>
2020-09-01 19:02:49 -0300

    Paginação com parametros front

--- src/main/java/com/lucas/curso/services/CategoriaService.java
+++ src/main/java/com/lucas/curso/services/CategoriaService.java
@@ -1,9 +1,13 @@
 package com.lucas.curso.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.lucas.curso.domain.Categoria;
@@ -42,5 +46,16 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não e possivel excluir uma Categoria com associações "); 
 		}
 	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
 
+	public Page<Categoria>  findPage(Integer page ,Integer linesPerPage , String orderBy ,String direction ){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		
+		return repo.findAll(pageRequest); 
+	}
 }
